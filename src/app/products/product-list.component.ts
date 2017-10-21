7732faaf50c1ffc54b244e04bca1b63c95af56dd
Grzegorz Swierczek <g.swierczek@gmail.com>
2017-10-21 19:20:07 +0200

    Product list displayed

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,63 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  imageWith: number = 50;
+  imageMargin: number = 2;
+  products: any[] = [
+    {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://1o9ddb39vxx9vbisv3djd3iysr.wpengine.netdna-cdn.com/wp-content/uploads/2017/06/Vinnie-Tortorich.jpg"
+    },
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/04/11/14/man-suit-istock-peopleimages.jpg"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "http://static4.businessinsider.com/image/58da7dee9cdcd341028b49be/22-clothing-items-every-man-should-own-before-he-turns-30.jpg"
+    },
+    {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "http://www.careers.man.eu/images/man-arbeitgeber/man_karriere_benefits_l.png"
+    },
+    {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "http://d279m997dpfwgl.cloudfront.net/wp/2017/05/headshot.jpeg"
+    }
+  ]
+}
